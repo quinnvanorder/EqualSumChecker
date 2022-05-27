ec78be943e5af75142819e0326eb3075f13b63c6
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-27 11:01:44 -0700

    Exercise completed!

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+Write a method hasEqualSum with 3 parameters of type int.
+
+The method should return boolean and it needs to return true if the sum of the first and second parameter is equal to
+the third parameter. Otherwise, return false.
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(EqualSumChecker.hasEqualSum(1,1,2));
+    }
+}
